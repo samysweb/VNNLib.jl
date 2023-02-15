6ec8f0788f5276550709db2848bb8aaec960ee98
Samuel Teuber <teuber@kit.edu>
2023-02-15 10:41:34 +0100

    Updated build proto

--- deps/build_proto.jl
+++ deps/build_proto.jl
@@ -1,5 +1,9 @@
 using ProtoBuf
 
+# Clone repository
+run(`git clone https://github.com/onnx/onnx.git $(@__DIR__)/onnx`)
 
 # ,"onnx/onnx-data.proto","onnx/onnx-operators.proto"
 protojl(["onnx/onnx.proto"], (@__DIR__)*"/onnx/",(@__DIR__)*"/../src/NNLoader")
+
+run(`rm -rf $(@__DIR__)/onnx`)
